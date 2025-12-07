35f4fe953a9004b9959103a68360943198bdae80
roshstha2006 <roshstha2006@gmail.com>
2025-12-07 19:03:09 +0545

    Add files via upload

---
+++ calculate.py
@@ -0,0 +1,20 @@
+# Basic Calculator by Roshan
+
+print("\n Calculator")
+
+num1 = float(input("Enter first number: "))
+op = input("Enter operator (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+
+if op == "+":
+    print("Result:", num1 + num2)
+elif op == "-":
+    print("Result:", num1 - num2)
+elif op == "*":
+    print("Result:", num1 * num2)
+elif op == "/":
+    print("Result:", num1 / num2 if num2 != 0 else "Can't divide by zero!")
+else:
+    print("Invalid operator!")
+
+print("\nCalculation completed!")
